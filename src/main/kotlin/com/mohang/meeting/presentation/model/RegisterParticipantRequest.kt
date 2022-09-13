0046858b5425796573ff966feeb0195675117a6c
shinD <huipulci1@naver.com>
2022-09-13 17:29:59 +0900

    [FIX] [#2] 모임 가입 승인 request 데이터 바인딩 안되는 오류 수정

--- src/main/kotlin/com/mohang/meeting/presentation/model/RegisterParticipantRequest.kt
+++ src/main/kotlin/com/mohang/meeting/presentation/model/RegisterParticipantRequest.kt
@@ -3,6 +3,7 @@ package com.mohang.meeting.presentation.model
 import com.mohang.meeting.application.usecase.participant.dto.CreateParticipantDto
 import com.mohang.meeting.application.usecase.participant.dto.SaveWrittenApplyFormDto
 import com.mohang.meeting.application.usecase.participant.dto.WrittenApplyFormDto
+import javax.validation.constraints.NotBlank
 
 /**
  * Created by ShinD on 2022/09/13.
@@ -13,12 +14,12 @@ data class RegisterParticipantRequest(
     val memberId: Long,
 
     // 모임에서의 별명
-    val nickname: String,
+    @field:NotBlank val nickname: String,
 
-    // 모임에서 사용할 프사 url
-    val profileImagePath: String? = null,
+    // 모임에서 사용할 프사 url (없는 경우 "")
+    val profileImagePath: String,
 
-    val applyFormId: Long? = null, // 해당 작성된 신청서는 어느 신청양식을 작성한 것인지
+    val applyFormId: Long = -1, // 해당 작성된 신청서는 어느 신청양식을 작성한 것인지 (없는 경우 -1)
 
     val writtenApplyFormFields: List<WrittenApplyFormRequest> = listOf(),
 
@@ -28,30 +29,32 @@ data class RegisterParticipantRequest(
         return CreateParticipantDto(
             memberId = memberId,
             nickname = nickname,
-            profileImagePath = profileImagePath
+            profileImagePath = profileImagePath.ifBlank { null }
         )
     }
 
     fun toServiceWrittenApplyFormDto(): SaveWrittenApplyFormDto? {
 
-        return applyFormId?.let {
+        // 신청 양식의 id가 1 이상이면서, 필드가 모두 작성된 경우
+        return if(applyFormId >= 1 && writtenApplyFormFields.isNotEmpty() ) {
 
             val writtenApplyFormDtos = writtenApplyFormFields.map { it.toServiceDto() }
 
-            SaveWrittenApplyFormDto(
+             SaveWrittenApplyFormDto(
                 memberId = memberId,
                 applyFormId = applyFormId,
                 writtenApplyFormFields = writtenApplyFormDtos,
             )
         }
+        else null
     }
 }
 
 data class WrittenApplyFormRequest(
 
-    val applyFormFieldId: Long, // 어느 신청 양식 필드에 대한 작성값인지
+    @field:NotBlank val applyFormFieldId: Long, // 어느 신청 양식 필드에 대한 작성값인지
 
-    val content: String, // 작성된 내용
+    @field:NotBlank val content: String, // 작성된 내용
 ) {
 
     fun toServiceDto(): WrittenApplyFormDto {
